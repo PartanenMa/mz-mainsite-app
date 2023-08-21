da0d9b68847024d1f7b55ae491f5e87bbb581f4b
Manu Partanen <manu111.partanen@gmail.com>
2023-08-21 07:00:33 +0300

    Fixed admin routing to the interests and hobbies.

--- MatrixZone-UI/src/Container/InterestProgrammingPage/ProgrammingPageAdmin.jsx
+++ MatrixZone-UI/src/Container/InterestProgrammingPage/ProgrammingPageAdmin.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import LoginFirstScreen from "/src/Components/LoginFirstScreen/LoginFirstScreen.jsx";
+import LoadingScreen from "/src/Components/LoadingScreen/LoadingScreen.jsx";
 import { info } from "/src/Constants/Info.jsx";
 import { motion, AnimatePresence } from "framer-motion";
 import "./ProgrammingPage.css";
@@ -7,6 +9,9 @@ import "./ProgrammingPage.css";
 function ProgrammingPageAdmin() {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(true);
+    const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+    const load = sessionStorage.getItem("load");
+    const [loading, setLoading] = useState(true);
 
     const toggleContainer = () => {
         setIsOpen(!isOpen);
@@ -16,35 +21,70 @@ function ProgrammingPageAdmin() {
         }, 500);
     };
 
-    return (
-        <AnimatePresence>
-            <div className="ProgrammingPageContent">
-                <motion.div
-                    className="ProgrammingContainer"
-                    initial={{ height: "0%" }}
-                    animate={{ height: isOpen ? "100%" : "0%" }}
-                    exit={{ height: "0%" }}
-                    transition={{ duration: 0.5 }}
-                    exitBeforeEnter
-                >
-                    <div className="ProgrammingTitle">
-                        <h2>Programming</h2>
-                    </div>
-                    <div className="Programming">
-                        <p>vgsfbhndhfm...</p>
-                    </div>
-                    <div className="Back">
-                        <button
-                            className="BackButton"
-                            onClick={toggleContainer}
-                        >
-                            Back
-                        </button>
-                    </div>
-                </motion.div>
+    useEffect(() => {
+        if (isLoggedIn === "true") {
+            //Simulate loading for 1 second:
+            const timer = setTimeout(() => {
+                setLoading(false);
+                if (load === "true") {
+                    sessionStorage.setItem("load", "false");
+                    notification.success({
+                        message: "LOGGED IN!",
+                        description: "Welcome back Admin.",
+                        placement: "bottomLeft",
+                        style: {
+                            backgroundColor: "lightgreen",
+                            border: "3px solid green",
+                        },
+                    });
+                }
+            }, 1000);
+
+            //Clean up the timer to prevent memory leaks:
+            return () => clearTimeout(timer);
+        }
+    }, [isLoggedIn, load]);
+
+    if (isLoggedIn === "true") {
+        return (
+            <div>
+                {loading && load === "true" ? (
+                    //Loading component here:
+                    <LoadingScreen />
+                ) : (
+                    <AnimatePresence>
+                        <div className="ProgrammingPageContent">
+                            <motion.div
+                                className="ProgrammingContainer"
+                                initial={{ height: "0%" }}
+                                animate={{ height: isOpen ? "100%" : "0%" }}
+                                exit={{ height: "0%" }}
+                                transition={{ duration: 0.5 }}
+                                exitBeforeEnter
+                            >
+                                <div className="ProgrammingTitle">
+                                    <h2>Programming</h2>
+                                </div>
+                                <div className="Programming">
+                                    <p>vgsfbhndhfm...</p>
+                                </div>
+                                <div className="Back">
+                                    <button
+                                        className="BackButton"
+                                        onClick={toggleContainer}
+                                    >
+                                        Back
+                                    </button>
+                                </div>
+                            </motion.div>
+                        </div>
+                    </AnimatePresence>
+                )}
             </div>
-        </AnimatePresence>
-    );
+        );
+    } else {
+        return <LoginFirstScreen />;
+    }
 }
 
 export default ProgrammingPageAdmin;
