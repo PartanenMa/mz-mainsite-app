0d46792a9eeaa9506e1c4d9156215b17ae0488ef
Manu Partanen <manu111.partanen@gmail.com>
2023-08-25 21:48:01 +0300

    Added hero section title to the front page.

--- MatrixZone-UI/src/Container/FrontPage/FrontPage.jsx
+++ MatrixZone-UI/src/Container/FrontPage/FrontPage.jsx
@@ -67,53 +67,62 @@ function Header({ setIsModalOpen }) {
 function Main({ isModalOpen, setIsModalOpen }) {
     return (
         <div className="Main">
-            <AnimatePresence>
-                {isModalOpen && (
-                    <motion.div
-                        className="FPModal"
-                        initial={{ opacity: 0, y: -100 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -100 }}
-                    >
-                        <div className="FPAboutTitle">
-                            <h2>What is the MatrixZone?</h2>
-                            <button className="FPTitleX-button" onClick={() => setIsModalOpen(false)}>
-                                X
-                            </button>
-                        </div>
-                        <div className="FPAbout">
-                            <p>
-                                Welcome to the MatrixZone! Here, I invite you to delve into my
-                                <br />
-                                world of passion and creativity. As an enthusiast in the realm of software development,
-                                this platform serves as a window into my
-                                <br />
-                                programming journey.
-                                <br />
-                                <br />
-                                Explore my profile to discover the mind behind the projects that
-                                <br />
-                                blend imagination with ingenuity, and immerse yourself in my
-                                <br />
-                                diverse project portfolio, where innovation meets determination.
-                                <br />
-                                <br />
-                                From coding marvels to ingenious experiments, MatrixZone is a
-                                <br />
-                                testament to my journey of growth and exploration as a fellow
-                                <br />
-                                software developer.
-                            </p>
-                        </div>
-                        <div className="FPBack">
-                            <button className="FPBackButton" onClick={() => setIsModalOpen(false)}>
-                                Close
-                            </button>
-                        </div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-            <section className="HeroSection"></section>
+            <section className="HeroSection">
+                <AnimatePresence>
+                    {isModalOpen && (
+                        <motion.div
+                            className="FPModal"
+                            initial={{ opacity: 0, y: -100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -100 }}
+                        >
+                            <div className="FPAboutTitle">
+                                <h2>What is the MatrixZone?</h2>
+                                <button className="FPTitleX-button" onClick={() => setIsModalOpen(false)}>
+                                    X
+                                </button>
+                            </div>
+                            <div className="FPAbout">
+                                <p>
+                                    Welcome to the MatrixZone! Here, I invite you to delve into my
+                                    <br />
+                                    world of passion and creativity. As an enthusiast in the realm of software
+                                    development, this platform serves as a window into my
+                                    <br />
+                                    programming journey.
+                                    <br />
+                                    <br />
+                                    Explore my profile to discover the mind behind the projects that
+                                    <br />
+                                    blend imagination with ingenuity, and immerse yourself in my
+                                    <br />
+                                    diverse project portfolio, where innovation meets determination.
+                                    <br />
+                                    <br />
+                                    From coding marvels to ingenious experiments, MatrixZone is a
+                                    <br />
+                                    testament to my journey of growth and exploration as a fellow
+                                    <br />
+                                    software developer.
+                                </p>
+                            </div>
+                            <div className="FPBack">
+                                <button className="FPBackButton" onClick={() => setIsModalOpen(false)}>
+                                    Close
+                                </button>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                <div class="HeroTitle">
+                    <h2 contenteditable>Welcome to the MatrixZone</h2>
+                    <p>My personal website</p>
+                    <p>
+                        Made with love using <span>React</span>
+                    </p>
+                </div>
+                <div class="HeroContent"></div>
+            </section>
         </div>
     );
 }
