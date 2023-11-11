e016decf634bdfb6b731240ca5c7e9bed2afe36a
Manu Partanen <manu111.partanen@gmail.com>
2023-11-11 18:38:16 +0200

    Data.jsx update.

--- src/Constants/Info.jsx
+++ src/Constants/Info.jsx
@@ -1,7 +1,7 @@
 import { data } from "./Data.jsx";
 
 export const info = {
-    version: "Alpha 0.7.4",
+    version: "Alpha 0.7.5",
     routes: {
         frontPage: "/",
         loginPage: "/login",
