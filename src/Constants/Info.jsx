758cad8917e3b0387571a9ffe621bc0da80aab2d
Manu Partanen <manu111.partanen@gmail.com>
2023-12-18 14:20:49 +0200

    R 1.0.7: Styling fixes.

--- src/Constants/Info.jsx
+++ src/Constants/Info.jsx
@@ -1,7 +1,7 @@
 import { data } from "./Data.jsx";
 
 export const info = {
-    version: "Release 1.0.6",
+    version: "Release 1.0.7",
     routes: {
         frontPage: "/MatrixZone-UI/",
         loginPage: "/MatrixZone-UI/login",
