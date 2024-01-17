35a6bddf2c4cdc1d300529aa30fda8f225475213
Manu Partanen <manu111.partanen@gmail.com>
2024-01-17 14:10:47 +0200

    R 1.0.9: Carousel styling fix.

--- src/Constants/Info.jsx
+++ src/Constants/Info.jsx
@@ -1,7 +1,7 @@
 import { data } from "./Data.jsx";
 
 export const info = {
-    version: "Release 1.0.8",
+    version: "Release 1.0.9",
     routes: {
         frontPage: "/MatrixZone-UI/",
         loginPage: "/MatrixZone-UI/login",
