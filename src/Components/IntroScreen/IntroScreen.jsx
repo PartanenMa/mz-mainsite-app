aeff6ef8030bf897eabc553daeca3295aa69f591
Manu Partanen <manu111.partanen@gmail.com>
2024-03-18 22:51:37 +0200

    R 1.0.36: Intro screen mobile screen added.

--- src/Components/IntroScreen/IntroScreen.jsx
+++ src/Components/IntroScreen/IntroScreen.jsx
@@ -55,55 +55,141 @@ function IntroScreen({ isIntroScreenOpen, setIsIntroScreenOpen }) {
                 <>
                     <MatrixBG />
                     <div className="introScreenOverlay" />
-                    <AnimatePresence>
-                        <motion.div className="introScreen" style={introScreenStyle} key="introScreen" initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -100 }}>
-                            <div className="introScreenWelcomeTitle">
-                                <motion.h2 key="iswt-h2" initial={{ opacity: 0, x: -1000 }} animate={{ opacity: 1, x: 0, transition: { duration: 1 } }} exit={{ opacity: 0, x: 1000 }}>
+                    {window.innerWidth >= 1280 && (
+                        <AnimatePresence>
+                            <motion.div
+                                className="introScreen"
+                                style={introScreenStyle}
+                                key="introScreen"
+                                initial={{ opacity: 0, y: -100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -100 }}
+                            >
+                                <div className="introScreenWelcomeTitle">
+                                    <motion.h2 key="iswt-h2" initial={{ opacity: 0, x: -1000 }} animate={{ opacity: 1, x: 0, transition: { duration: 1 } }} exit={{ opacity: 0, x: 1000 }}>
+                                        Welcome to the{" "}
+                                        <motion.span className="iSWTName" key="iswtname" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 4 } }} exit={{ opacity: 0 }}>
+                                            MatrixZone
+                                        </motion.span>
+                                    </motion.h2>
+                                    <div className="iswtLogo" />
+                                </div>
+                                <div className="introScreenAbout">
+                                    <motion.h3 key="isa-h3" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0, transition: { duration: 1 } }} exit={{ opacity: 0, y: 100 }}>
+                                        My personal website <span style={{ color: "green", fontStyle: "normal" }}>{"(" + info.version + ")"}</span>
+                                    </motion.h3>
+                                    <motion.p key="isa-p" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0, transition: { duration: 1 } }} exit={{ opacity: 0, y: 100 }}>
+                                        POWERED BY:
+                                    </motion.p>
+                                    <motion.div
+                                        className="pL-Logo"
+                                        key="pllogo"
+                                        initial={{ opacity: 0, y: 100 }}
+                                        animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                                        exit={{ opacity: 0, y: 100 }}
+                                    />
+                                    <motion.div className="poweredBy" key="poweredby" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 3 } }} exit={{ opacity: 0 }}>
+                                        {technologiesUsed.length > 0 ? (
+                                            technologiesUsed.map((tech, index) =>
+                                                info.api.enabled ? (
+                                                    <div className="techUsed" key={index} style={{ "--techUsed-color": tech.color, textDecoration: "none" }}>
+                                                        <div className="techUsedTitle">
+                                                            <h5 style={{ color: tech.color }}>{tech.name}</h5>
+                                                        </div>
+                                                        <div className="techUsedLogo" style={{ backgroundImage: `url(${tech.image})`, backgroundSize: tech.size }} />
+                                                    </div>
+                                                ) : index === 0 || index === 1 ? (
+                                                    <div className="techUsed" key={index} style={{ "--techUsed-color": tech.color, textDecoration: "none" }}>
+                                                        <div className="techUsedTitle">
+                                                            <h5 style={{ color: tech.color }}>{tech.name}</h5>
+                                                        </div>
+                                                        <div className="techUsedLogo" style={{ backgroundImage: `url(${tech.image})`, backgroundSize: tech.size }} />
+                                                    </div>
+                                                ) : null
+                                            )
+                                        ) : (
+                                            <div className="noTechUsedData">
+                                                <h4>NO DATA!</h4>
+                                            </div>
+                                        )}
+                                    </motion.div>
+                                </div>
+                                <div className="introScreenEnter">
+                                    <motion.button
+                                        className="introScreenEnterButton"
+                                        onClick={() => closeIntroScreen()}
+                                        key="introscreenenterbutton"
+                                        initial={{ opacity: 0, y: -100 }}
+                                        animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                                        exit={{ opacity: 0, y: -100 }}
+                                        whileHover={{
+                                            scale: 1.05,
+                                            transition: { duration: 0.1 },
+                                        }}
+                                        whileTap={{ scale: 0.9 }}
+                                    >
+                                        Enter
+                                    </motion.button>
+                                </div>
+                            </motion.div>
+                        </AnimatePresence>
+                    )}
+                    {window.innerWidth <= 500 && (
+                        <div className="introScreenMobile">
+                            <div className="introScreenWelcomeTitleMobile">
+                                <motion.h2 key="iswt-h2m" initial={{ opacity: 0, x: -1000 }} animate={{ opacity: 1, x: 0, transition: { duration: 1 } }} exit={{ opacity: 0, x: 1000 }}>
                                     Welcome to the{" "}
-                                    <motion.span className="iSWTName" key="iswtname" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 4 } }} exit={{ opacity: 0 }}>
+                                    <motion.span className="iSWTNameM" key="iswtnamem" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 4 } }} exit={{ opacity: 0 }}>
                                         MatrixZone
                                     </motion.span>
                                 </motion.h2>
                                 <div className="iswtLogo" />
                             </div>
-                            <div className="introScreenAbout">
-                                <motion.h3 key="isa-h3" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0, transition: { duration: 1 } }} exit={{ opacity: 0, y: 100 }}>
+                            <div className="introScreenAboutMobile">
+                                <motion.h3 key="isa-h3m" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0, transition: { duration: 1 } }} exit={{ opacity: 0, y: 100 }}>
                                     My personal website <span style={{ color: "green", fontStyle: "normal" }}>{"(" + info.version + ")"}</span>
                                 </motion.h3>
-                                <motion.p key="isa-p" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0, transition: { duration: 1 } }} exit={{ opacity: 0, y: 100 }}>
+                                <motion.p key="isa-pm" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0, transition: { duration: 1 } }} exit={{ opacity: 0, y: 100 }}>
                                     POWERED BY:
                                 </motion.p>
-                                <motion.div className="madeWith" key="madewith" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 3 } }} exit={{ opacity: 0 }}>
+                                <motion.div
+                                    className="pL-LogoM"
+                                    key="pllogom"
+                                    initial={{ opacity: 0, y: 100 }}
+                                    animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                                    exit={{ opacity: 0, y: 100 }}
+                                />
+                                <motion.div className="poweredByM" key="poweredbym" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 3 } }} exit={{ opacity: 0 }}>
                                     {technologiesUsed.length > 0 ? (
                                         technologiesUsed.map((tech, index) =>
                                             info.api.enabled ? (
-                                                <div className="techUsed" key={index} style={{ "--techUsed-color": tech.color, textDecoration: "none" }}>
-                                                    <div className="techUsedTitle">
+                                                <div className="techUsedM" key={index} style={{ "--techUsed-color": tech.color, textDecoration: "none" }}>
+                                                    <div className="techUsedTitleM">
                                                         <h5 style={{ color: tech.color }}>{tech.name}</h5>
                                                     </div>
-                                                    <div className="techUsedLogo" style={{ backgroundImage: `url(${tech.image})`, backgroundSize: tech.size }} />
+                                                    <div className="techUsedLogoM" style={{ backgroundImage: `url(${tech.image})`, backgroundSize: tech.size }} />
                                                 </div>
                                             ) : index === 0 || index === 1 ? (
-                                                <div className="techUsed" key={index} style={{ "--techUsed-color": tech.color, textDecoration: "none" }}>
-                                                    <div className="techUsedTitle">
+                                                <div className="techUsedM" key={index} style={{ "--techUsed-color": tech.color, textDecoration: "none" }}>
+                                                    <div className="techUsedTitleM">
                                                         <h5 style={{ color: tech.color }}>{tech.name}</h5>
                                                     </div>
-                                                    <div className="techUsedLogo" style={{ backgroundImage: `url(${tech.image})`, backgroundSize: tech.size }} />
+                                                    <div className="techUsedLogoM" style={{ backgroundImage: `url(${tech.image})`, backgroundSize: tech.size }} />
                                                 </div>
                                             ) : null
                                         )
                                     ) : (
-                                        <div className="noTechUsedData">
+                                        <div className="noTechUsedDataM">
                                             <h4>NO DATA!</h4>
                                         </div>
                                     )}
                                 </motion.div>
                             </div>
-                            <div className="introScreenBack">
+                            <div className="introScreenEnterMobile">
                                 <motion.button
-                                    className="introScreenBackButton"
+                                    className="introScreenEnterButtonM"
                                     onClick={() => closeIntroScreen()}
-                                    key="introscreenbackbutton"
+                                    key="introscreenenterbuttonm"
                                     initial={{ opacity: 0, y: -100 }}
                                     animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
                                     exit={{ opacity: 0, y: -100 }}
@@ -116,8 +202,8 @@ function IntroScreen({ isIntroScreenOpen, setIsIntroScreenOpen }) {
                                     Enter
                                 </motion.button>
                             </div>
-                        </motion.div>
-                    </AnimatePresence>
+                        </div>
+                    )}
                 </>
             )}
         </>,
